d693cdf83fcd79bb7a4300b9393627df58371bbb
bello maryam <bellomaryam@ymail.com>
2020-04-16 13:07:12 +0100

    course list page

--- js/custom.js
+++ js/custom.js
@@ -27,98 +27,20 @@ $(document).ready(function () {
 
     function slideToggleIcons() {
         $('.program-details').click(function () {
-            // if ($('.accordion-divs').is(':hidden')) {
-            // if ($('.accordion-divs').is(':hidden')) {
             $(this).toggleClass('rotate-icon-plus');
             $('.rotate-icon-plus'). not(this).removeClass('rotate-icon-plus')
-                // console.log('hjbhgrgiu')
-                // if ($(this).next('.accordion-divs').is(':hidden')) {
-                    
-                    // if ($('.accordion-divs').is('.collapse:not(.show)')) {
-                    //     $('.program-details').toggleClass('rotate-icon-plus')
-                    // }
-                // }
-                // else {
-                //     $(this).toggleClass('rotate-icon-plus')
-                // }    
-            // }
-            // else {
-            //     console.log('accordion-divs');
-            //     $('.accordion-divs').removeClass('rotate-icon-plus')
-                
-            // }   
-
-
-            // if ($('.accordion-divs').is('.collapse:not(.show)')) {
-            //     $(this).toggleClass('rotate-icon-plus')
-            // }
-
-            // if ($('.accordion-divs').is('.collapse:not(.show)')) {
-            //         $('.program-details').toggleClass('rotate-icon-plus')
-            //     }
-                // $('.program-details').toggleClass('rotate-icon-plus')
-            
-
-            // if (!$('.program-details').is(':focus')) {
-            //     $('.program-details').toggleClass('rotate-icon-plus')
-            // }
-            // else {
-            //         $(this).toggleClass('rotate-icon-plus')
-            // }
-                // if (!$('.program-details').is(':focus')) {
-                //     $('.program-details').toggleClass('rotate-icon-plus')
-                // }
-                
-            // }
-            // else {
-                // $(this).removeClass('rotate-icon-plus');
-                // $(this).addClass('rotate-icon-minus');
-            // }
-            // if ($('.accordion-divs').is(':visible')) {
-            //     $('.program-details').toggleClass('rotate-icon-minus');
-            // }
-
-            
-            // else {
-                
-            // }
-
-            
-
-            // if ($('.accordion-divs').is(':visible')) {
-            //     $('.program-details').toggleClass('rotate-icon-plus')
-            // }
-            // else {
-            //     $('.program-details').toggleClass('rotate-icon-plus');
-            // }
-            // }
-
-            // else  {
-            //     $(this).children('i.fa-minus-circle').toggleClass('rotate-icon-minus');
-            //     $(this).children('i.fa-plus-circle').show();
-            //     // $('i.fa-minus').hide();
-            //     // });
-            // }
         })
     }
     slideToggleIcons()
 
-  
+    $('.hover-this-div').hover(function () {
+    //   alert('ghfsd')
+        $(this).prev('div').find('img').animate({ opacity: 1.0 }, 100);
+    }, function () {
+            $(this).prev('div').find('img').animate({ opacity: 0.3}, 100);
+        
+    })
 
-    // $('a.program-details').click(function () {
-    //     var children = $(this).children();
-    //     if (children.hasClass('fa-plus')) {
-    //         $('.fa-plus').slideToggle(function () {
-    //             $('.fa-minus').show()
-    //         });
-    //     }
-    //     else if (children.hasClass('fa-minus')) {
-    //         $('.fa-minus').slideToggle(function () {
-    //             $('.fa-plus').show();
-    //             $('.fa-minus').hide();
-    //         });
-    //     }  
-    // })
 
 
 });
